ad9fb2ba3e82c5e6229a06ec3633513de2b66e9f
Vincent Slingsby <particlehavoc@live.ca>
2018-07-19 16:45:04 -0700

    get() methods for database calls now function correctly

--- KPU Faculty Scheduler/DBMethods.cs
+++ KPU Faculty Scheduler/DBMethods.cs
@@ -73,65 +73,92 @@ CREATE TABLE professorscourses(
             SQLiteCommand cmd = connection.CreateCommand();
             cmd.CommandText = "select * from professorsclass where teachID = " + teachID + " and classID = " + classID;
             //select the row where the teacher id and class id are shared
-            SQLiteDataReader data = cmd.ExecuteReader();
-            return data.HasRows; //if that row exists, then the class is taught by the teacher - so you return true
+            using (SQLiteDataReader data = cmd.ExecuteReader())
+            {
+                while(data.Read())
+                {
+                    return data.HasRows; //if that row exists, then the class is taught by the teacher - so you return true
+                }
+                
+            }
+            return false;
         }
         public Room getRoom(int id)
         {
             SQLiteCommand cmd = connection.CreateCommand();
             cmd.CommandText = "select * from rooms where id = " + id;
-            SQLiteDataReader data = cmd.ExecuteReader();
             Room room = new Room();
-            room.id = data.GetInt32(0);
-            room.name = data.GetString(1);
-            room.hasComputers = (data.GetInt32(3) == 1);
-            return room;
+            using (SQLiteDataReader data = cmd.ExecuteReader())
+            {
+                while (data.Read())
+                {
+                    room.id = data.GetInt32(0);
+                    room.name = data.GetString(1);
+                    room.hasComputers = (data.GetInt32(3) == 1);
+                    return room;
+                }
+                return null;
+            }
+                
             //return new Room(data.GetInt32(0), data.GetString(1), data.GetBoolean(3));
         }
         public Course getCourse(int id)
         {
             SQLiteCommand cmd = connection.CreateCommand();
             cmd.CommandText = "select * from rooms where id = " + id;
-            SQLiteDataReader data = cmd.ExecuteReader();
             Course course = new Course();
-            course.id = data.GetInt32(0);
-            course.name = data.GetString(1);
-            course.needsComputers = (data.GetInt32(3) == 1);
-            return course;
+            using (SQLiteDataReader data = cmd.ExecuteReader())
+            {
+                while (data.Read())
+                {
+                    course.id = data.GetInt32(0);
+                    course.name = data.GetString(1);
+                    course.needsComputers = (data.GetInt32(3) == 1);
+                    return course;
+                }
+            }
+            return null;
+                
             //return new Room(data.GetInt32(0), data.GetString(1), data.GetBoolean(3));
         }
         public CourseBlock getBlock(int id)
         {
             SQLiteCommand cmd = connection.CreateCommand();
             cmd.CommandText = "select * from schedule where id = " + id;
-            SQLiteDataReader data = cmd.ExecuteReader();
             CourseBlock block = new CourseBlock();
-            block.id = id;
-            block.professor = getProfessor(data.GetInt32(1));
-            block.course = getCourse(data.GetInt32(2));
-            block.room = getRoom(data.GetInt32(3));
-            block.time = data.GetInt32(4);
-            return block;
+            using (SQLiteDataReader data = cmd.ExecuteReader())
+            {
+                while (data.Read())
+                {
+                    block.id = id;
+                    block.professor = getProfessor(data.GetInt32(1));
+                    block.course = getCourse(data.GetInt32(2));
+                    block.room = getRoom(data.GetInt32(3));
+                    block.time = data.GetInt32(4);
+                    return block;
+                }
+            }
+            return null;
         }
 
         public void addProfessor(Professor prof)
         {
             SQLiteCommand cmd = connection.CreateCommand();
-            cmd.CommandText = "INSERT INTO professors (id,name) values (" + prof.id + "," + "'" + prof.name + "'" + ");";
+            cmd.CommandText = "INSERT INTO professors (id,name) values (" + prof.id + ",'" + prof.name + "');";
             cmd.ExecuteNonQuery();
         }
         public void addRoom(Room room)
         {
             int computers = room.hasComputers ? 1 : 0; //room has computers? yes = 1, no = 0
             SQLiteCommand cmd = connection.CreateCommand();
-            cmd.CommandText = "INSERT INTO rooms (id,name,hasComputers) values (" + room.id + "," + room.name + "," + computers + ");";
+            cmd.CommandText = "INSERT INTO rooms (id,name,hasComputers) values (" + room.id + ",'" + room.name + "'," + computers + ");";
             cmd.ExecuteNonQuery();
         }
         public void addCourse(Course course)
         {
             int computers = course.needsComputers ? 1 : 0; //room has computers? yes = 1, no = 0
             SQLiteCommand cmd = connection.CreateCommand();
-            cmd.CommandText = "INSERT INTO courses (id,name,sections,needsComputers) values (" + course.id + "," + course.name + "," + course.sections + "," + computers + ");";
+            cmd.CommandText = "INSERT INTO courses (id,name,sections,needsComputers) values (" + course.id + ",'" + course.name + "'," + course.sections + "," + computers + ");";
             cmd.ExecuteNonQuery();
         }
         public void addCanTeach(int profid, int courseid)
