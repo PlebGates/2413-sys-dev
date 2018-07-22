7a33abf54ac2651964d3d4c4b82676f3be518a92
Vincent Slingsby <particlehavoc@live.ca>
2018-07-22 13:05:16 -0700

    Revert "room() changes and db connection"
    This reverts commit fa450616fe5151e9684d2a51d46e1190eb3c59e6.

--- KPU Faculty Scheduler/Room.cs
+++ KPU Faculty Scheduler/Room.cs
@@ -11,10 +11,11 @@ namespace KPU_Faculty_Scheduler
 
         // Variables to hold the room name and id.
         public int id;
-        public string name; 
+        public int roomNum;
+        public String building; 
 
         // Boolean variable for whether the room has a computer lab or not.
-        public bool hasComputers;
+        public Boolean hasComputers;
 
         // Default Constructor.
         public Room()
@@ -23,10 +24,11 @@ namespace KPU_Faculty_Scheduler
         }
 
         // Main Constructor.
-        public Room(int id_, string name_, bool hasComputers_)
+        public Room(int id_, int roomNum_, String building_, Boolean hasComputers_)
         {
             id = id_;
-            name = name_;
+            roomNum = roomNum_;
+            building = building_;
             hasComputers = hasComputers_;
         }
 
